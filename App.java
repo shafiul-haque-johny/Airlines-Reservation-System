e2321f5e51615fb8c741beda79f10a40d2433da0
Shafiul Haque Johny <71293557+shafiul-haque-johny@users.noreply.github.com>
2021-03-02 21:31:27 +0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,34 @@
+package app;
+
+// Java program to implement solution of producer 
+// consumer problem. 
+import java.util.LinkedList; 
+import java.util.Random;
+
+public class App 
+{ 
+	public static void main(String[] args) 
+						throws InterruptedException 
+	{
+            
+           
+	} 
+
+    
+
+}
+
+
+class global{
+    
+    public static String from="";
+    public static String to="";
+    public static String date="";
+    public static String Class="";
+    public static String adult="";
+    public static String infant="";
+    public static String child="";
+    
+    public static int flight_number=0; 
+    public static int total_price=0;
+}
